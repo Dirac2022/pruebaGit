1c9abd07f308ea3ac17766a65c2b840933e941be
Mitchel <daronmitchel@gmail.com>
2023-08-31 11:45:33 -0500

    Version 1.1

--- src/persona/Persona.java
+++ src/persona/Persona.java
@@ -23,10 +23,12 @@ obtenerEdad (2pts)
 public class Persona {
     
     String nombre;
+    String apellido;
     int edad;
     String genero;
     static int edadAcumulada = 0;
     static int cantidadPersonas = 0;
+    
     
     public Persona(){
        cantidadPersonas ++;
@@ -49,6 +51,14 @@ public class Persona {
     
     String obtenerNombre(){
         return nombre;
+    }
+    
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
     }
     
     void setearEdad(int edad){
